56e64e385a4f79f1d111c819b08c5c9078e02178
Justin Poirier <110320397+jcpoirier20@users.noreply.github.com>
2023-06-27 13:59:56 -0500

    Added react router. Added NotFound page. Implement stickyNav.

--- client/components/StickyNavBar.tsx
+++ client/components/StickyNavBar.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import AddIcon from '@mui/icons-material/Add';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from 'react-router-dom';
+
+// uses icons to navigate between home page, API search page, Account info Page
 const StickyNavBar = () => {
+
+
+  return (
+    <div id="stick-nav-bar">
+      <Link to="../home">
+        <HomeIcon />
+      </Link>
+      <Link to="../add">
+        <AddIcon />
+      </Link>
+      <Link to="../account">
+        <AccountCircleIcon />
+      </Link>
+    </div>
+  )
 
 };
 
